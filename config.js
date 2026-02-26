a14f36beef0f6cf959f404177c71beea3da5f30e
Richard <richard@example.com>
2026-02-26 03:44:32 +0000

    security: remove config.js from git (contains secrets)

--- config.js
+++
@@ -1,8 +0,0 @@
-// Configuration - DO NOT COMMIT TO VERSION CONTROL
-// Copy this file as config.js and keep secure
-
-const CONFIG = {
-  SUPABASE_URL: 'https://tjebidvgvbpnxgnphcrg.supabase.co',
-  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqZWJpZHZndmJwbnhnbnBoY3JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0ODkyNDcsImV4cCI6MjA4NzA2NTI0N30.7MyRW330dP1ZkCqOqTykjRo6Y-cM4ow3aJ-6_H82BUY',
-  APP_PASSWORD: 'WillsX2025!'
-};
